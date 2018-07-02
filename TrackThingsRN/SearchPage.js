6141a0c409b72afd2eacf401282926365d4a69da
aaron-wilson <aaron-wilson@users.noreply.github.com>
2018-07-02 00:16:00 -0400

    working on tutorial

--- TrackThingsRN/SearchPage.js
+++ TrackThingsRN/SearchPage.js
@@ -11,23 +11,72 @@ import {
   Image,
 } from 'react-native';
 
+function urlForQueryAndPage(key, value, pageNumber) {
+  const data = {
+      country: 'uk',
+      pretty: '1',
+      encoding: 'json',
+      listing_type: 'buy',
+      action: 'search_listings',
+      page: pageNumber,
+  };
+  data[key] = value;
+
+  const querystring = Object.keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&');
+
+  return 'https://api.nestoria.co.uk/api?' + querystring;
+}
+
 export default class SearchPage extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {
+	    searchString: '',
+	    isLoading: false,
+	  };
+	}
+
+	_onSearchStringChanged = (event) => {
+	  console.log('_onSearchStringChanged');
+	  this.setState({ searchString: event.nativeEvent.text });
+	  console.log('Current: '+this.state.searchString+', Next: '+event.nativeEvent.text);
+	};
+
+	_executeQuery = (query) => {
+	  console.log(query);
+	  this.setState({ isLoading: true });
+	};
+
+	_onGoPressed = () => {
+	  const query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+	  this._executeQuery(query);
+	};
+
   render() {
+  	console.log('SearchPage.render');
+  	const spinner = this.state.isLoading ?
+  		<ActivityIndicator size='large' /> : null;
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
-          Search for houses to buy!
+          Find your dream home.
         </Text>
         <View style={styles.flowRight}>
 				  <TextInput
 				    style={styles.searchInput}
-				    placeholder='Enter name or zip code' />
+				    value={this.state.searchString}
+				    onChange={this._onSearchStringChanged}
+				    placeholder='Enter city or ZIP code' />
 				  <Button
-				    onPress={() => {}}
+				    onPress={this._onGoPressed}
 				    color='#48BBEC'
 				    title='Go'
 				  />
 				</View>
+				<Image source={require('./Resources/house/house.png')} style={styles.image}/>
+				{spinner}
       </View>
     );
   }
@@ -61,5 +110,10 @@ const styles = StyleSheet.create({
 	  borderRadius: 8,
 	  color: '#48BBEC',
 	  textAlign: 'center',
+	},
+	image: {
+		marginTop: 20,
+	  width: 217,
+	  height: 138,
 	},
 });
